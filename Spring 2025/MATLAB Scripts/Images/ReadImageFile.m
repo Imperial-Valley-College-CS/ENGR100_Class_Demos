1aed75834a3f9078f8188aa2653cf927264c2383
ortoct <104336410+ortoct@users.noreply.github.com>
2025-04-09 12:21:06 -0700

    Update ReadImageFile.m

--- Spring 2025/MATLAB Scripts/Images/ReadImageFile.m
+++ Spring 2025/MATLAB Scripts/Images/ReadImageFile.m
@@ -17,7 +17,21 @@ red = img(:,:,1);
 %create 3D matrix with red layer only (zero green, zero blue)
 red_layer = cat(3,red,justZeros,justZeros);
 
+%extract green matrix from image (3D matrix)
+gre = img(:,:,2);
+%create 3D matrix with green layer only (zero red, zero blue)
+gre_layer = cat(3,justZeros,gre,justZeros);
+
+%extract blue matrix from image (3D matrix)
+blu = img(:,:,3);
+%create 3D matrix with blue layer only (zero red, zero green)
+blu_layer = cat(3,justZeros,justZeros,blu);
+
 figure
 imshow(img)
 figure
 imshow(uint8(red_layer))
+figure
+imshow(uint8(gre_layer))
+figure
+imshow(uint8(blu_layer))
