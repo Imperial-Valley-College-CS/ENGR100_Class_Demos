e43768d924f5884730f574586ffb7930fc4692a3
ortoct <104336410+ortoct@users.noreply.github.com>
2025-04-30 11:35:43 -0700

    Update SensorsForMotors.ino

--- Spring 2025/Arduino Scripts/SensorsForMotors/SensorsForMotors.ino
+++ Spring 2025/Arduino Scripts/SensorsForMotors/SensorsForMotors.ino
@@ -2,16 +2,29 @@ int leftPhotoResPin = A1;
 int rightPhotoResPin = A0;
 int speedPin = A5;
 
-int leftSenor;      //voltage at left most sensor
+int leftSensor;      //voltage at left most sensor
 int rightSensor;    //voltage at right most sensor
 int speedSensor;    //voltage for speed sensor
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(leftPhotoResPin, INPUT);
+  pinMode(rightPhotoResPin, INPUT);
+  pinMode(speedPin, INPUT);
+  Serial.begin(9600);
+  delay(1500);    //1.5 second delay for motor startup
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  leftSensor = analogRead( leftPhotoResPin );
+  rightSensor = analogRead( rightPhotoResPin );
+  speedSensor = analogRead( speedPin );
 
+  Serial.print("Left Sensor: " + leftSensor);
+  Serial.print(", Rigth Sensor: " + rightSensor);
+  Serial.print(", Speed Sensor: " + speedSensor);
+  Serial.println();
+  delay(500);
+  
 }
